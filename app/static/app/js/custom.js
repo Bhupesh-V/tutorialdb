9c618d30da349b35c54c5da9f427fb4bb5bb7437
Bhupesh-V <varshneybhupesh@gmail.com>
2019-07-27 13:20:37 +0530

    applied some patches - v1.1.1 - auto-scroll to search results. - auto focus on contribute page. - remove duplicate JS code.

---
+++ app/static/app/js/custom.js
@@ -0,0 +1,25 @@
+
+setTimeout(function focusMe(){
+  document.getElementById("focus-here").scrollIntoView();
+});
+
+function share(title, link){
+  if (navigator.share) {
+    navigator.share({
+    title: title,
+    url: link,
+    })
+    .then(() => console.log('Successful Share'))
+    .catch((error) => console.log('Error sharing', error));
+  }
+}
+
+async function registerSW(){
+  if('serviceWorker' in navigator) {
+    try{
+        await navigator.serviceWorker.register("{% static 'app/js/sw.js' %}");
+    } catch(e) {
+      console.log('SW Registration Failed.');
+    }
+  }
+}
