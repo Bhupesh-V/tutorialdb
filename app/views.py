ef2d93898db7e3025a43c6705ff7da1be7af1ca6
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-08-09 00:14:11 +0530

    Add Titles
    Added 'title' varible in  base.html template.
    Added 'title' key in most contexts.
    Added docstring to the views.

--- app/views.py
+++ app/views.py
@@ -7,87 +7,113 @@ from taggie.parser import generateTags
 import time
 
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+	"""
+	Home page view.
+	"""
+	template_name = 'home.html'
 
 def latest(request):
-    results = tutorial.objects.all().order_by('-id')[:10]
-    results = { 'results': results }
-    return render(request, 'latest.html', results)
-
+	"""
+	View for the latest tutorial entries.
+	"""
+	results = tutorial.objects.all().order_by('-id')[:10]
+	context = {'results': results, 'title': 'Latest'}
+	return render(request, 'latest.html', context)
 
 def about(request):
-    return render(request, 'about.html')
-
+	"""
+	About view.
+	"""
+	return render(request, 'about.html', {'title': 'About'})
 
 def search_query(request):
-    query = request.GET.get('q').lower()
-    category = request.GET.get('category')
-    list_query = query.split()
+	"""
+	View for the search results.
+	"""
+	query = request.GET.get('q').lower()
+	category = request.GET.get('category')
+	list_query = query.split()
 
-    start_time = time.time()
+	start_time = time.time()
 
-    if category is not None:
-        object_list = tutorial.objects.filter(
-            (Q(title__icontains=query) | Q(tags__name__in=list_query)) & Q(category__icontains=category)
-        ).order_by('id').distinct()
-    else:
-        object_list = tutorial.objects.filter(
-            (Q(title__icontains=query) & Q(tags__name__in=list_query))
-        ).order_by('id').distinct()
-    end_time = time.time()
-    total = len(object_list)
-    result_time = round(end_time - start_time, 3)
+	if category is not None:
+		object_list = tutorial.objects.filter(
+			(Q(title__icontains=query) | Q(tags__name__in=list_query)) & Q(category__icontains=category)
+		).order_by('id').distinct()
+	else:
+		object_list = tutorial.objects.filter(
+			(Q(title__icontains=query) & Q(tags__name__in=list_query))
+		).order_by('id').distinct()
+	end_time = time.time()
+	total = len(object_list)
+	result_time = round(end_time - start_time, 3)
 
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        object_list = paginator.page(page)
-    except PageNotAnInteger:
-        object_list = paginator.page(1)
-    except EmptyPage:
-        object_list = paginator.page(paginator.num_pages)
+	paginator = Paginator(object_list, 3)
+	page = request.GET.get('page')
+	try:
+		object_list = paginator.page(page)
+	except PageNotAnInteger:
+		object_list = paginator.page(1)
+	except EmptyPage:
+		object_list = paginator.page(paginator.num_pages)
 
-    context = {'tquery':query, 'object_list':object_list, 'total': total, 'time': result_time}
+	context = {
+		'tquery':query, 
+		'object_list':object_list, 
+		'total': total, 
+		'time': result_time, 
+		'title': query
+	}
 
-    return render(request, 'search_results.html', context)
-
+	return render(request, 'search_results.html', context)
 
 class ContributeView(TemplateView):
-    def get(self, request):
-        return render(request, 'contribute.html')
-    
-    def post(self, request):
-        linkCount = tutorial.objects.filter(link = request.POST['tlink']).count()
-        if linkCount == 0:
-            tags, title = generateTags(request.POST['tlink'])
-            if 'other' in tags:
-                return render(request, 'contribute.html', {'error': "Not a Tutorial Link, Try Again"})
-            else:
-                tutorialObject = tutorial.objects.create(
-                    title = title, 
-                    link = request.POST['tlink'], 
-                    category = request.POST['tcategory']
-                )
-                for t in tags:
-                    obj, created = tag.objects.get_or_create(name=t)
+	"""
+	View for the tutorial contribution page.
+	"""
+	def get(self, request):
+		"""
+		GET the contribution form.
+		"""
+		return render(request, 'contribute.html')
+	
+	def post(self, request):
+		"""
+		POST a tutorial.
+		"""
+		linkCount = tutorial.objects.filter(link = request.POST['tlink']).count()
+		if linkCount == 0:
+			tags, title = generateTags(request.POST['tlink'])
+			if 'other' in tags:
+				return render(request, 'contribute.html', {'error': "Not a Tutorial Link, Try Again"})
+			else:
+				tutorialObject = tutorial.objects.create(
+					title = title, 
+					link = request.POST['tlink'], 
+					category = request.POST['tcategory']
+				)
+				for t in tags:
+					obj, created = tag.objects.get_or_create(name=t)
 
-                tagObjList = tag.objects.filter(name__in = tags)
-                tutorialObject.tags.set(tagObjList)
-                return render(request, 'thankyou.html')
-        return render(request, 'thankyou.html')
-
+				tagObjList = tag.objects.filter(name__in = tags)
+				tutorialObject.tags.set(tagObjList)
+				return render(request, 'thankyou.html')
+		return render(request, 'thankyou.html')
 
 def tags(request):
-    object_list = tag.objects.all()
-    context = {'object_list':object_list}
-
-    return render(request, 'tags.html', context)
-
+	"""
+	View for the tags.
+	"""
+	object_list = tag.objects.all()
+	context = {'object_list':object_list, 'title': 'Tags'}
+	return render(request, 'tags.html', context)
 
 def taglinks(request, tagname):
-    taglist = []
-    taglist.append(tagname)
-    object_list = tutorial.objects.filter(tags__name__in = taglist)
-    context = {'tag': tagname, 'object_list':object_list}
-
-    return render(request, 'taglinks.html', context)
+	"""
+	View for the tutorials with the {tagname}.
+	"""
+	taglist = []
+	taglist.append(tagname)
+	object_list = tutorial.objects.filter(tags__name__in = taglist)
+	context = {'tag': tagname, 'object_list':object_list, 'title': tagname}
+	return render(request, 'taglinks.html', context)
