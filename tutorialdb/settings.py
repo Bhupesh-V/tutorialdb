5e440b2344d0532357e31c808304bd3de9ce3ca1
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-08-25 20:43:43 +0530

    Update settings.py
    Commented the SECRET_KEY.

--- tutorialdb/settings.py
+++ tutorialdb/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
