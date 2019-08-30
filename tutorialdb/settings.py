cbe4c50d6536c23e545b7ca08164c4a59fcddd5e
Animesh-Ghosh <34956994+Animesh-Ghosh@users.noreply.github.com>
2019-08-30 20:09:36 +0530

    Update settings and README
    Uncommented the SECRET_KEY and LOCAL_HOST environment variables.
    Updated how to setup environment variables under Installation instructions.
    Added PowerShell commands for creating virtual environment under Installation instructions.

--- tutorialdb/settings.py
+++ tutorialdb/settings.py
@@ -6,13 +6,13 @@ load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    # os.environ['LOCAL_HOST'], # your local IP to test the site on your network,
+    os.environ['LOCAL_HOST'], # your local IP to test the site on your network,
     'tutorialdb.pythonanywhere.com',
 ]
 
