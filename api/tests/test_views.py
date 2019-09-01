4019d7a88b9570e1c9cc2a9e0cff2c5ab24297b8
Bhupesh-V <varshneybhupesh@gmail.com>
2019-09-01 12:49:52 +0530

    more tests #9

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -4,7 +4,7 @@ from django.test import TransactionTestCase
 class APITests(TransactionTestCase):
 
     def test_tutorials_page_status_code(self):
-        response = self.client.get('/tutorials')
+        response = self.client.get('/tutorials/')
         self.assertEquals(response.status_code, 200)
 
     def test_tags_page_status_code(self):
--- app/tests/test_views.py
+++ app/tests/test_views.py
@@ -29,3 +29,31 @@ class DynamicPageTests(TransactionTestCase):
     def test_tags_page_status_code(self):
         response = self.client.get('/tags/')
         self.assertEquals(response.status_code, 200)
+
+
+class TestTemplateNames(TransactionTestCase):
+
+    def test_home_page_name(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(template_name='home.html')
+
+    def test_api_page_name(self):
+        response = self.client.get('/api/')
+        self.assertTemplateUsed(template_name='api.html')
+
+    def test_about_page_name(self):
+        response = self.client.get('/about/')
+        self.assertTemplateUsed(template_name='about.html')
+
+    def test_contribute_page_name(self):
+        response = self.client.get('/contribute/')
+        self.assertTemplateUsed(template_name='contribute.html')
+
+    def test_latest_page_name(self):
+        response = self.client.get('/latest/')
+        self.assertTemplateUsed(template_name='latest.html')
+
+    def test_tags_page_name(self):
+        response = self.client.get('/tags/')
+        self.assertTemplateUsed(template_name='tags.html')
+
