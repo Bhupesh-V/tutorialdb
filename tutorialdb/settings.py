6a74ae119c18779b9a4bb4e23e4b4ac6fdb79883
Lior Shub <liorbentov@gmail.com>
2019-10-10 22:34:30 +0300

    minor

--- tutorialdb/settings.py
+++ tutorialdb/settings.py
@@ -118,9 +118,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = (os.path.join('static'), )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
