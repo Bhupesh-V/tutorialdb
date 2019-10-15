b71429b30657ad265eb0ae8e39555b22c9eb4f94
Bhupesh-V <varshneybhupesh@gmail.com>
2019-10-15 17:59:15 +0530

    release v1.1.4

--- tutorialdb/settings.py
+++ tutorialdb/settings.py
@@ -118,8 +118,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
