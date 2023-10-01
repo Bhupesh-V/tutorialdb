025d1ebb9011f34396524e9e977be77a04ab2e21
aashish <aashujain095@gmail.com>
2023-10-01 19:09:44 +0530

    fix:Static Files Bug when running 'manage.py test' for setup #52

---
+++ staticfiles/app/js/custom.js
@@ -0,0 +1,25 @@
+setTimeout(function focusMe() {
+    const elem = document.getElementById("focus-here");
+    if (elem) {
+        elem.scrollIntoView();
+    }
+});
+
+function share(title, link) {
+	if (navigator.share) {
+		navigator.share({
+		title: title,
+		url: link,
+		})
+		.then(() => console.log('Successful Share'))
+		.catch((error) => console.log('Error sharing', error));
+	}
+}
+
+function checkEmpty() {
+	var input = document.getElementById("search-bar");
+	if (input.value === "" || input.value === null) {
+		return false;
+	}
+	return true;
+}
